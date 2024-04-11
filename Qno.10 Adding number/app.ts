105e6b48afec3cf4e25fc9ef573e5028b2ef9572
Nehal Gohar <148964031+Nehalgohar962@users.noreply.github.com>
2024-04-11 10:36:46 -0700

    Delete Qno.10 Adding number directory

--- Qno.10 Adding number/app.ts
+++
@@ -1,5 +0,0 @@
-//  Question no 10 Adding number
-// My name s Laiba gohar
-// Dated : 20 - 3 - 2024
-
-console.log("Hello world simple program");
--- Qno.10 Adding number/app2.ts
+++
@@ -1,6 +0,0 @@
-//  Question no 10 Adding number
-// My name s Laiba gohar
-// Dated : 20 - 3 - 2024
-// This program will run simple code just like Hello world
-
-console.log(5 * 2);
